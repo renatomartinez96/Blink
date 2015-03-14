155cfd6e877c0cb33c53304eafb7c9cbe4eecf6f
glopez97 <gerardo.antonio97@gmail.com>
2015-03-14 17:38:28 -0600

    href profile

--- nav/sidebar.php
+++ nav/sidebar.php
@@ -11,7 +11,7 @@
 <div id="sidebar-wrapper" style="background-color:#BF5A16;">
 	<ul class="sidebar-nav toggled">
     	<li class="sidebar-brand">
-            <a href='#'><img class='toggled' src='../assets/img/avatares/<?=$avatar?>.png' id='avatar'></a>
+            <a href='profile.php'><img class='toggled' src='../assets/img/avatares/<?=$avatar?>.png' id='avatar'></a>
         </li>
         <li>
         	<a class="iconos" href="#"><i class="fa fa-home fa-2x"></i><b class="textos toggled" href="#"> Inicio</b></a>
