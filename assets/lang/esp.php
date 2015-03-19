88a5f51bb595d6c2c77d0d83e491a318c1b41739
glopez97 <gerardo.antonio97@gmail.com>
2015-03-18 18:00:24 -0600

    idioma

---
+++ assets/lang/esp.php
@@ -0,0 +1,51 @@
+<!--
+
+Copyright (c) 2015 Blink
+All Rights Reserved
+ 
+This product is protected by copyright and distributed under
+licenses restricting copying, distribution, and decompilation.
+
+Gerardo López | Iván Graciano | Renato Andres
+
+-->
+
+<?php 
+//Archivo de traduccion en idioma español
+$lang = array(
+//----------------------------------------------
+    
+    
+//  Navbar
+//      boton cerrar sesion
+        "navbar-1" => "Cerrar Sesion",
+
+// /# Navbar
+//----------------------------------------------
+
+    
+//  estudiante
+//      Pantalla de perfil
+        "profile-1" => "Informacion",
+        "profile-2" => "Nombre",
+        "profile-3" => "Correo Electronico",
+        "profile-4" => "Fecha de nacimiento",
+        "profile-5" => "Descripcion",
+        "profile-6" => "Tipo de usuario",
+//   /# Pantalla de perfil
+// /# estudiante
+//----------------------------------------------
+
+
+//    administrador
+
+// /# administrador
+//----------------------------------------------
+
+
+//    docente
+
+// /# docente
+//----------------------------------------------
+);
+?>
