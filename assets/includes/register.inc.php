324acf468bee874ef2b8fb447d89ac7cf664ec13
glopez97 <gerardo.antonio97@gmail.com>
2015-04-01 11:27:10 -0600

    registro con token

--- assets/includes/register.inc.php
+++ assets/includes/register.inc.php
@@ -47,9 +47,8 @@ if (isset($_POST['username'], $_POST['email'], $_POST['p'], $_POST['name'], $_PO
         if ($stmt->num_rows == 1) {
             // A user with this email address already exists
             $error_msg .= '<p class="error">A user with this email address already exists.</p>';
-                        $stmt->close();
+            $stmt->close();
         }
-                $stmt->close();
     } else {
         $error_msg .= '<p class="error">Database error Line 39</p>';
                 $stmt->close();
@@ -89,8 +88,73 @@ if (isset($_POST['username'], $_POST['email'], $_POST['p'], $_POST['name'], $_PO
         $password = hash('sha512', $password . $random_salt);
  
         // Insert the new user into the database 
-        if ($insert_stmt = $mysqli->prepare("INSERT INTO usuarios_tb (nombres, apellidos, nacimiento, usuario, correo, contra, estado, tipo, salt, log) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")) {
-            $insert_stmt->bind_param('ssssssssss',$name, $last, $date, $username, $email, $password, $estado, $tipo, $random_salt, $log);
+        if ($insert_stmt = $mysqli->prepare("INSERT INTO usuarios_tb (nombres, apellidos, nacimiento, usuario, correo, contra, estado, token, tipo, salt, log) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")) {
+            $token = md5(substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 15).$username);
+            $insert_stmt->bind_param('sssssssssss',$name, $last, $date, $username, $email, $password, $estado, $token, $tipo, $random_salt, $log);
+            /**
+             * This example shows settings to use when sending via Google's Gmail servers.
+             */
+
+            //SMTP needs accurate times, and the PHP time zone MUST be set
+            //This should be done in your php.ini, but this is how to do it if you don't have access to that
+            date_default_timezone_set('Etc/UTC');
+
+            require 'phpmailer/PHPMailerAutoload.php';
+
+            //Create a new PHPMailer instance
+            $mail = new PHPMailer;
+
+            //Tell PHPMailer to use SMTP
+            $mail->isSMTP();
+
+            //Enable SMTP debugging
+            // 0 = off (for production use)
+            // 1 = client messages
+            // 2 = client and server messages
+            $mail->SMTPDebug = 2;
+
+            //Ask for HTML-friendly debug output
+            $mail->Debugoutput = 'html';
+
+            //Set the hostname of the mail server
+            $mail->Host = 'smtp.gmail.com';
+
+            //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+            $mail->Port = 587;
+
+            //Set the encryption system to use - ssl (deprecated) or tls
+            $mail->SMTPSecure = 'tls';
+
+            //Whether to use SMTP authentication
+            $mail->SMTPAuth = true;
+
+            //Username to use for SMTP authentication - use full email address for gmail
+            $mail->Username = "blink.project2015@gmail.com";
+
+            //Password to use for SMTP authentication
+            $mail->Password = "blink2015";
+
+            //Set who the message is to be sent from
+            $mail->setFrom('blink.project2015@gmail.com', 'Blink Project');
+
+            //Set an alternative reply-to address
+            $mail->addReplyTo('blink.project2015@gmail.com', 'Blink Project');
+
+            //Set who the message is to be sent to
+            $mail->addAddress($email, $name.' '.$last);
+
+            //Set the subject line
+            $mail->Subject = 'Activacion de cuenta';
+
+            //Read an HTML message body from an external file, convert referenced images to embedded,
+            //convert HTML into a basic plain-text alternative body
+            $mail->msgHTML("
+                <h1>ENHORABUENA!</h1>
+                <h3>Te has registrado exitosamente en nuestra plataforma. Para empezar a recibir todos los beneficios que Blink ofrece, es necesario que actives tu cuenta.</h3>
+                <p>Inicia sesion e introduce el siguiente codigo</p>
+                <p>$token</p>
+            ");
+            $mail->send();
             // Execute the prepared query.
             if (!$insert_stmt->execute()) {
                 header('Location: ../error.php?err=Registration failure: INSERT');
