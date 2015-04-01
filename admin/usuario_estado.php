5c8545ac0368b9a284b09d5976e362ed8bbae824
elCherre <ignh.lel@yandex.com>
2015-04-01 15:46:46 -0600

    Validaciones del admin
    Todo infalible ;)

--- admin/usuario_estado.php
+++ admin/usuario_estado.php
@@ -3,6 +3,8 @@ include_once '../assets/includes/db_conexion.php';
 include_once '../assets/includes/funciones.php';
  
 sec_session_start();
+include 'auto.php';
+$user = $_SESSION['username'];
 ?>
 <?php
 ?>
@@ -53,7 +55,7 @@ Gerardo López | Iván Nolasco | Renato Andres
 if(isset($_GET["id"]))
 {
     $c=$_GET["id"];
-    $query = "SELECT idusuario, estado FROM usuarios_tb WHERE idusuario = $c";
+    $query = "SELECT idusuario, estado, nombres, apellidos FROM usuarios_tb WHERE idusuario = $c";
     if ($query = mysqli_query($mysqli, $query)) 
     {
         $num = mysqli_num_rows($query);
@@ -62,16 +64,18 @@ if(isset($_GET["id"]))
             $row = mysqli_fetch_assoc($query);
             $id = $row["idusuario"];
             $est = $row["estado"];
+            $nombre = $row["nombres"];
+            $apellido = $row["apellidos"];
             if($est == 0)
             {
                 $query2 = "UPDATE usuarios_tb SET estado = 1 WHERE idusuario='$id'";
                 if ($query2 = mysqli_query($mysqli, $query2)) 
                 {
-                    die("<p class='text-warning'>User deactivated <a href='usuarios.php'>Back to the list</a></p>");
+                    die("<div class='alert alert-warning'><strong><i class='fa fa-check'></i> OK:</strong> The status of the user: &quot;".$nombre." ".$apellido."&quot; has been changed to:  <strong>Inactive</strong>. <a href='usuarios.php' class='alert-link'>Back to the list of users</a>.</div>");
                 }
                 else
                 {
-                    die("<p class='text-danger'>ERROR: In the data <a href='usuarios.php'>Back to the list</a></p>");
+                    die("<div class='alert alert-danger'><strong><i class='fa fa-times'></i> ERROR:</strong> In the data <a href='usuarios.php' class='alert-link'>Back to the list of users</a>.</div>");
                 }
             }
             elseif($est == 1)
@@ -79,11 +83,11 @@ if(isset($_GET["id"]))
                 $query3 = "UPDATE usuarios_tb SET estado = 0 WHERE idusuario='$id'";
                 if ($query3 = mysqli_query($mysqli, $query3)) 
                 {
-                    die ("<p class='text-success'>User activated <a href='usuarios.php'>Back to the list</a></p>");
+                    die ("<div class='alert alert-success'><strong><i class='fa fa-check'></i> Perfect:</strong> The status of the user: &quot;".$nombre." ".$apellido."&quot; has been changed to:  <strong>Active</strong>. <a href='usuarios.php' class='alert-link'>Back to the list of users</a>.</div>");
                 }
                 else
                 {
-                    die("<p class='text-danger'>ERROR: In the data <a href='usuarios.php'>Back to the list</a></p>");
+                    die("<div class='alert alert-danger'><strong><i class='fa fa-times'></i> ERROR:</strong> In the data <a href='usuarios.php' class='alert-link'>Back to the list of users</a>.</div>");
                 }
             }
         }
--- admin/usuarios.php
+++ admin/usuarios.php
@@ -3,6 +3,8 @@ include_once '../assets/includes/db_conexion.php';
 include_once '../assets/includes/funciones.php';
  
 sec_session_start();
+include 'auto.php';
+$user = $_SESSION['username'];
 ?>
 <?php
 //$stmt = $mysqli->prepare("SELECT idusuario, nombres, apellidos, nacimiento, genero, usuario, estado, correo, tipo, lang FROM usuarios_tb");
@@ -63,7 +65,68 @@ Gerardo López | Iván Nolasco | Renato Andres
             <div id="page-content-wrapper">
 				<div class="container-fluid">
 					<div class="row">
-                        <h2 class="junction-regular text-center">List of users</h2>
+<?php
+$titulo = "";
+if(isset($_GET["tipo"]))
+{
+    $usutipo = $_GET["tipo"];
+    switch($usutipo){
+    case 1;
+    $query = "SELECT idusuario, nombres, apellidos, nacimiento, usuario, estado, correo, tipo FROM usuarios_tb WHERE tipo = 1";
+    $titulo = "Listado de administradores de Blink";
+    break;
+    case 2;
+    $query = "SELECT idusuario, nombres, apellidos, nacimiento, usuario, estado, correo, tipo FROM usuarios_tb WHERE tipo = 2";
+    $titulo = "Listado de profesores de Blink";
+    break;
+    case 3;
+    $query = "SELECT idusuario, nombres, apellidos, nacimiento, usuario, estado, correo, tipo FROM usuarios_tb WHERE tipo = 3";
+    $titulo = "Listado de estudiantes de Blink";
+    break;
+    }
+}
+else
+{
+    if(isset($_GET["type"]))
+    {
+        $usutipo2 = $_GET["type"];
+        switch($usutipo2){
+        case 1;
+        $query = "SELECT idusuario, nombres, apellidos, nacimiento, usuario, estado, correo, tipo FROM usuarios_tb WHERE tipo = 1";
+        $titulo = "List of a";
+        break;
+        case 2;
+        $query = "SELECT idusuario, nombres, apellidos, nacimiento, usuario, estado, correo, tipo FROM usuarios_tb WHERE tipo = 2";
+        $titulo = "Lissgknfkgndkf";
+        break;
+        case 3;
+        $query = "SELECT idusuario, nombres, apellidos, nacimiento, usuario, estado, correo, tipo FROM usuarios_tb WHERE tipo = 3";
+        $titulo = "aroougnd frj worl";
+        break;
+        }
+    }
+    else
+    {
+        $query = "SELECT idusuario, nombres, apellidos, nacimiento, usuario, estado, correo, tipo FROM usuarios_tb";
+        $titulo = "Listado de todos los usuarios de Blink";
+    }
+}
+?>
+                        <div style="float:left; font-size: 80%; position: relative; top:20px; left:15px;"><a href="javascript:history.back();" class="btn btn-info btn-sm"><i class="fa fa-arrow-left"></i> Volver</a></div>
+                        <h2 class="junction-regular text-center"><?=$titulo?></h2>
+                        <div class="dropdown">
+                        <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
+                        Clasificar la lista por: 
+                        <span class="caret"></span>
+                        </button>
+                        <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+                            <li role="presentation"><a role="menuitem" tabindex="-1" href="usuarios.php?tipo=1">Administradores</a></li>
+                            <li role="presentation"><a role="menuitem" tabindex="-1" href="usuarios.php?tipo=2">Profesores</a></li>
+                            <li role="presentation"><a role="menuitem" tabindex="-1" href="usuarios.php?tipo=3">Estudiantes</a></li>
+                            <li role="presentation" class="divider"></li>
+                            <li role="presentation"><a role="menuitem" tabindex="-1" href="usuarios.php">Todos los usuarios</a></li>
+                        </ul>
+                        </div>
                         <table class="table table-hover table-responsive">
                         <thead>
                             <tr><th>ID</th>
@@ -79,8 +142,6 @@ Gerardo López | Iván Nolasco | Renato Andres
                         </thead>
                         <tbody>
 <?php
-$query = "SELECT idusuario, nombres, apellidos, nacimiento, usuario, estado, correo, tipo FROM usuarios_tb";
-
 if ($result = mysqli_query($mysqli, $query)) 
 {
     while ($row = mysqli_fetch_assoc($result)) 
@@ -106,12 +167,12 @@ if ($result = mysqli_query($mysqli, $query))
             case 0;
             $text2 = "<i class='fa fa-check'></i> Active";
             $tbclass = "";
-            $text3 = "<a href='usuario_editar.php?id=".$row["idusuario"]."' class='btn btn-info btn-xs'><i class='fa fa-pencil'></i> Edit</a><a href='usuario_estado.php?id=".$row["idusuario"]."' class='btn btn-danger btn-xs'><i class='fa fa-user-times'></i> Deactivate</a>";
+            $text3 = "<a href='usuario_editar.php?id=".$row["idusuario"]."' class='btn btn-info btn-xs'><i class='fa fa-pencil'></i> Edit</a><a href='usuario_estado.php?id=".$row["idusuario"]."' class='btn btn-danger btn-xs' onClick=\"alert('Are you sure to change the user status?')\"><i class='fa fa-user-times'></i> Deactivate</a>";
             break;
             case 1;
             $text2 = "<i class='fa fa-user-times'></i> Inactive";
             $tbclass = "class='text-danger'";
-            $text3 = "<a href='usuario_editar.php?id=".$row["idusuario"]."' class='btn btn-info btn-xs' disabled='isabled'><i class='fa fa-pencil'></i> Edit</a><a href='usuario_estado.php?id=".$row["idusuario"]."' class='btn btn-success btn-xs'><i class='fa fa-check'></i> Activate</a>";
+            $text3 = "<a href='usuario_editar.php?id=".$row["idusuario"]."' class='btn btn-info btn-xs' disabled='isabled'><i class='fa fa-pencil'></i> Edit</a><a href='usuario_estado.php?id=".$row["idusuario"]."' class='btn btn-success btn-xs' onClick=\"alert('Are you sure to change the user status?')\"><i class='fa fa-check'></i> Activate</a>";
             break;
         }
         echo "<tr ".$tbclass."><td>".$row["idusuario"]."</td>";
@@ -134,7 +195,9 @@ if ($result = mysqli_query($mysqli, $query))
         </div>
         </div>
         <!--Main js-->
-		
+		<?php 
+			include 'main_js.php';
+		?>
 		<!--/#Main js-->
 	</body>
 </html>
