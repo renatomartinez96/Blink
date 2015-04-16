aef893d2cfb48805c37c284e07ae5596918e843e
glopez97 <gerardo.antonio97@gmail.com>
2015-04-15 22:03:08 -0600

    navbar

--- nav/sidebar.php
+++ nav/sidebar.php
@@ -1,26 +1,52 @@
+<div id="sidebar-wrapper" style="background-color:#BF5A16;">
+	<ul class="sidebar-nav toggled">
+        <li class="sidebar-brand">
+            <a href='profile.php'><img class='toggled' src='../assets/img/avatares/<?=$avatar?>.png' id='avatar'></a>
+        </li>
 <?php
     $avatar = '';
-    if ($stmt = $mysqli->prepare("SELECT avatar FROM usuarios_tb WHERE usuario = ?")) {
+    if ($stmt = $mysqli->prepare("SELECT tipo, avatar FROM usuarios_tb WHERE usuario = ?")) {
         $stmt->bind_param('s', $user);
         $stmt->execute(); 
         $stmt->store_result();
-        $stmt->bind_result($avatar);
+        $stmt->bind_result($tipo, $avatar);
         $stmt->fetch();
     }
+    switch($tipo){
+        case 1:
+//        ADMIN
+        ?>
+            <li>
+            </li>
+            <li>
+            </li>
+            <li>
+            </li>
+        <?php
+        break;
+        case 2:
+//        TEACHER
+        ?>
+            <li>
+            </li>
+        <?php
+        break;
+        case 3:
+//        STUDENT
+        ?>
+            <li>
+            </li>
+            <li>
+            </li>
+            <li>
+            </li>
+        <?php
+        break;
+    }
 ?>
-<div id="sidebar-wrapper" style="background-color:#BF5A16;">
-	<ul class="sidebar-nav toggled">
-    	<li class="sidebar-brand">
-            <a href='profile.php'><img class='toggled' src='../assets/img/avatares/<?=$avatar?>.png' id='avatar'></a>
-        </li>
-        <li>
-        	<a class="iconos" href="index.php"><i class="fa fa-home fa-2x"></i><b class="textos toggled" href="#"> Home</b></a>
-        </li>
-        <li>
-        	<a class="iconos" href="index1.php"><i class="fa fa-file-code-o fa-2x"></i><b class="textos toggled" href="#"> Page</b></a>
-        </li>
-        <li>
-        	<a class="iconos" href="teachers.php"><i class="fa fa-users fa-2x"></i><b class="textos toggled" href="#"> Teachers</b></a>
-        </li>
+
+
+    	
+        
 	</ul>
 </div>
