511272cd44aad4d5a8455a87ec6af5c6ffda415d
Gerardo Antonio López Barrientos <gerardo.antonio97@gmail.com>
2015-04-17 13:07:21 -0600

    mail

--- lel.php
+++ lel.php
@@ -1,22 +1,36 @@
 <?php
-require 'assets/includes/phpmailer/class.phpmailer.php';
-require 'assets/includes/phpmailer/class.smtp.php';
-$mail = new PHPMailer();
-$mail->IsSMTP();
-$mail->SMTPDebug  = 2;                    
-$mail->SMTPAuth   = true;               
-$mail->Host       = "a2plcpnl0093.prod.iad2.secureserver.net";
-$mail->Port       = 465;              
-$mail->Username   = "no-reply@the-box.link"; 
-$mail->Password   = "5Zer%~2+Ew??";  
-$mail->SetFrom('no-reply@the-box.link', 'Box Link(Do not reply)');
+require_once('assets/includes/phpmailer/PHPMailerAutoload.php');
+require_once('assets/includes/phpmailer/class.phpmailer.php');
+require_once("assets/includes/phpmailer/class.smtp.php"); // optional, gets called from within class.phpmailer.php if not already loaded
+
+$mail = new PHPMailer(true); // the true param means it will throw exceptions on errors, which we need to catch
+$mail->IsSMTP(); // telling the class to use SMTP
+$mail->Host       = "mail.the-box.link"; // SMTP server
+$mail->SMTPDebug  = 2;                     // enables SMTP debug information (for testing)
+$mail->SMTPAuth   = true;                  // enable SMTP authentication
+$mail->Host       = "mail.the-box.link"; // sets the SMTP server
+$mail->Port       = 25;                    // set the SMTP port for the GMAIL server
+$mail->Username   = "no-reply@the-box.link"; // SMTP account username
+$mail->Password   = "5Zer%~2+Ew??";        // SMTP account password
 $mail->AddReplyTo('staff@the-box.link', 'Box Link Staff');
 $mail->AddAddress("gerardo.antonio97@gmail.com", "gerardo");
-$mail->Subject = 'PHPMailer Test Subject via mail(), advanced';
-$mail->msgHTML("
-<h1>ENHORABUENA!</h1>
-<h3>Te has registrado exitosamente en nuestra plataforma. Para empezar a recibir todos los beneficios que Blink ofrece, es necesario que actives tu cuenta.</h3>
-<p>Inicia sesion e introduce el siguiente codigo</p>
-<p>$token</p>
-"); 
-$mail->send();
+$mail->SetFrom('no-reply@the-box.link', 'Box Link(Do not reply)');
+$mail->Subject = 'Box Link - Restablecer contraseña';
+$mail->AltBody = 'To view the message, please use an HTML compatible email viewer!'; // optional - MsgHTML will create an alternate automatically
+$foto= "http://the-box.link/assets/img/restablecer.png";
+$mensaje='<a href="http://the-box.link"><img src="'. $foto .'">';
+$mail->Body = $mensaje;
+$mail -> charSet = "UTF-8";
+if($mail->Send())
+{
+  echo "Message Sent OK<p></p>\n";  
+}
+else
+{
+  echo "Message NOT Sent<p></p>\n";
+}
+
+
+
+    
+?>
--- mailpass.php
+++ mailpass.php
@@ -1,23 +1,39 @@
 <?php
 include_once 'assets/includes/db_conexion.php';
-require 'assets/includes/phpmailer/class.phpmailer.php';
-require 'assets/includes/phpmailer/class.smtp.php';
+require_once('assets/includes/phpmailer/PHPMailerAutoload.php');
+require_once('assets/includes/phpmailer/class.phpmailer.php');
+require_once("assets/includes/phpmailer/class.smtp.php");
 if (isset($_POST['email'])) 
 {
-    $stmt = $mysqli->prepare("SELECT correo, usuario FROM usuarios_tb WHERE correo = ?");
+    $stmt = $mysqli->prepare("SELECT correo, usuario, nombres, apellidos FROM usuarios_tb WHERE correo = ?");
     $stmt->bind_param('s', $_POST['email']);
     $stmt->execute(); 
     $stmt->store_result();
-    $stmt->bind_result($mail, $usuario);
+    $stmt->bind_result($mail, $usuario, $name, $last);
     $stmt->fetch();
     if ($stmt->num_rows == 1){
         $token = md5(substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 15).$usuario);
         $stmt1 = $mysqli->prepare("UPDATE usuarios_tb SET token = ? WHERE usuario = ?");
         $stmt1->bind_param('ss', $token, $usuario);
         if($stmt1->execute()){
-              
-             echo "Box Link have sent you an email. Please verifay your inbox at <strong>".$mail."</strong>.";
-            
+            $mail = new PHPMailer(true);
+            $mail -> charSet = "UTF-8";
+            $mail->IsSMTP();            
+            $mail->SMTPAuth   = true;                  
+            $mail->Host       = "mail.the-box.link"; 
+            $mail->Port       = 25;                   
+            $mail->Username   = "no-reply@the-box.link"; 
+            $mail->Password   = "5Zer%~2+Ew??";        
+            $mail->AddReplyTo('staff@the-box.link', 'Box Link Staff');
+            $mail->AddAddress($_POST['email'],$name);
+            $mail->SetFrom('no-reply@the-box.link', 'Box Link');
+            $mail->Subject = "Box Link - Restablecer contraseña";
+            $mail->AltBody = 'To view the message, please use an HTML compatible email viewer!'; 
+            $foto= "http://the-box.link/assets/img/restablecer.png";
+            $mensaje="<a href='http://the-box.link?t=".$token."'><img src='". $foto ."' style='width:100%;'>";
+            $mail->Body = $mensaje;
+            $mail->Send();
+            echo "Box Link have sent you an email. Please verifay your inbox at <strong>".$mail."</strong>."; 
         }
     }else{
         echo "Email not found. Please try again or register at Box Link";
