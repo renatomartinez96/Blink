5b2fee70eae5789e1b2380570f54216926aff967
elCherre <ignh.lel@yandex.com>
2015-05-14 01:40:05 -0600

    Funciona con mejoras
    :alien:

--- admin/personalizacion.php
+++ admin/personalizacion.php
@@ -9,14 +9,38 @@ if(isset($_POST["userid"]) && isset($_POST["banselect"]))
     $banner = $_POST["banselect"];
     $userid = $_POST["userid"];
 
-    $query = "INSERT INTO user_config VALUES ('', '$userid', '', '$banner') ";
-    if ($query = mysqli_query($mysqli, $query)) 
+    $query1 = "SELECT * FROM user_config WHERE iduser = $userid";
+    if ($query1 = mysqli_query($mysqli, $query1)) 
     {
-        echo "lel";
+        $nums = mysqli_num_rows($query1);
+        if($nums == 1)
+        {
+            $query2 = "UPDATE user_config SET banner='$banner' WHERE iduser = '$userid'";
+            if ($query2 = mysqli_query($mysqli, $query2)) 
+            {
+                echo "lel1";
+            }
+            else
+            {
+                echo "error1";
+            }
+        }
+        else
+        {
+            $query3 = "INSERT INTO user_config VALUES ('', '$userid', '', '$banner') ";
+            if ($query3 = mysqli_query($mysqli, $query3)) 
+            {
+                echo "lel2";
+            }
+            else
+            {
+                echo "error2";
+            }
+        }
     }
     else
     {
-        echo "lelakfsdfbsdfb";
+        echo "Error: de sql";
     }
 }
 else
