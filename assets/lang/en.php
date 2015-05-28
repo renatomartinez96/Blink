de6ec56da643a3cfbdf5974252b6a6f5e32f524b
elCherre <ignh.lel@yandex.com>
2015-05-28 11:33:29 -0600

    Nuevos archivos de traducciones
    Guardan las matrices de las frases en diferente idioma

---
+++ assets/lang/en.php
@@ -0,0 +1,110 @@
+<!--
+
+Copyright (c) 2015 Blink
+All Rights Reserved
+ 
+This product is protected by copyright and distributed under
+licenses restricting copying, distribution, and decompilation.
+
+Gerardo López | Iván Graciano | Renato Andres
+
+-->
+
+<?php 
+//Archivo de traduccion en idioma ingles
+$langprint = array(
+//----------------------------------------------
+// GLOBALES
+    // Diferentes textos para botones
+        "btn-cancel" => "Cancel",
+        "btn-save" => "Save",
+        "btn-next" => "Next",
+        "btn-save-changes" => "Save changes",
+        "btn-save-my-data" => "Save my data",
+    // /# Diferentes textos para botones 
+    //----------------------------------------------
+    // Diferentes mensajes de la plataforma
+        "msg-alert-saved-ok" => "<strong>Perfect! </strong>The data was saved correctly",
+        "msg-alert-saved-error1" => "<strong>Error: </strong>There is a server problem",
+    // /# Diferentes mensajes de la plataforma
+    //----------------------------------------------
+    //   modal de cambio de banner
+        "modal-banner-change-title" => "Change my banner",
+        "modal-banner-change-preview" => "Preview",
+        "modal-banner-change-brief" => "For editing your personal data, you must click <a href='my_data.php'>here</a>, and if you want change your code editor theme, go <a href='cod_theme.php'>here</a>.",
+        "modal-banner-change-tip" => "The Box Link Staff will put other banner soon...",
+    //  /# modal de cambio de banner
+    //----------------------------------------------  
+    // Formulario de cambio de datos "my_data.php"
+        "my_data-main-title" => "Box Link configuration",
+        "tab-public-data" => "Public data",
+        "tab-security" => "Security",
+        "tab-language" => "Language",
+        "content-p-d-name-field" => "Name",
+        "content-p-d-lastname-field" => "Lastname",
+        "content-p-d-description-field" => "Description",
+        "brief-p-d-title" => "Remember",
+        "brief-p-d-li-1" => "You only have a length of 50 characters for your name and your lastname",
+        "brief-p-d-li-2" => "For your description you have a length of 300 characters, please be terse as possible",
+    // /# Formulario de cambio de datos "my_data.php"
+    //----------------------------------------------
+    //  Navbar
+    //      boton cerrar sesion
+            "navbar-1" => "Logout",
+
+    // /# Navbar
+// /# GLOBALES
+//----------------------------------------------
+
+    
+//  estudiante
+//      Pantalla de perfil
+        "profile-1" => "Information",
+        "profile-2" => "Name",
+        "profile-3" => "E-mail",
+        "profile-4" => "Birth date",
+        "profile-5" => "Description",
+        "profile-6" => "Type of user",
+//   /# Pantalla de perfil
+// /# estudiante
+//----------------------------------------------
+
+
+//    administrador
+//      index
+    "user-page-link" => "Web site of",
+    "search-input" => "Looking for users, teachers, courses... ?",
+    "admin-friendly-intro" => "Welcome Administrator",
+    "admin-maintenance-users" => "Users",
+    "admin-maintenance-users-new-notification" => "New users:",
+    "admin-maintenance-users-no-notification" => "No new users",
+    "admin-maintenance-users-go-list" => "Go to the users list",
+    "admin-maintenance-users-list" => "List of registered users in Box Link",
+    "admin-maintenance-courses" => "Courses",
+    "admin-maintenance-courses-go-list" => "Go to the courses list",
+    "admin-maintenance-courses-list" => "List of teacher courses in Box Link",
+//      /# index
+// /# administrador
+//----------------------------------------------
+
+
+//    docente
+
+// /# docente
+//----------------------------------------------
+    
+
+//    prueba
+        "lel" => "The to&ntilde;ito :D",
+        "nosession" => "You are not logged in",
+        "welcomeblink" => "Welcome to BLink!",
+        "yonoespik" => "<p class='text-center'><strong><a href='index.php'><i class='fa fa-language'></i> ¡¿Qu&eacute; es esto?! Yo no hablo ingl&eacute;s</a></strong></p>",
+        "emailname" => "Email",
+        "emailexample" => "example@mail.com",
+        "contra" => "Password",
+        "entrarlog" => "¡Log in!",
+        "sincuenta" => "Still without an account? Create one here!",
+// /# prueba
+//----------------------------------------------
+);
+?>
